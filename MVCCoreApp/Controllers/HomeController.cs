87575ff3ae0d4f1ad1fcff274ac612d1b6f5d561
pawelniki <pawelniki@gmail.com>
2017-05-27 22:18:47 +0200

    Initial commit.

---
+++ MVCCoreApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCoreApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Title"] = "Moja pierwsza strona azure";
+            ViewData["Message"] = "Hej jest to moja pierwsza strona w Azure.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
