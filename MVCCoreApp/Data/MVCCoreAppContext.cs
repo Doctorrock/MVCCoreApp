636f3b4c1be073f242f504ae21dd4a5665bd6f5e
pawelniki <pawelniki@gmail.com>
2017-05-28 17:51:21 +0200

    added database contection

---
+++ MVCCoreApp/Data/MVCCoreAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCCoreApp.Models
+{
+    public class MVCCoreAppContext : DbContext
+    {
+        public MVCCoreAppContext (DbContextOptions<MVCCoreAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCCoreApp.Models.Person> Person { get; set; }
+    }
+}
---
+++ MVCCoreApp/Migrations/MVCCoreAppContextModelSnapshot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MVCCoreApp.Models;
+
+namespace MVCCoreApp.Migrations
+{
+    [DbContext(typeof(MVCCoreAppContext))]
+    partial class MVCCoreAppContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("MVCCoreApp.Models.Person", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("CreationTime");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Person");
+                });
+        }
+    }
+}
