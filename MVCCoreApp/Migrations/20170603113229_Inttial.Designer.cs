388cb91c112b7799f82695f6dc7034ac1bf84e7b
pawelniki <pawelniki@gmail.com>
2017-06-03 14:52:45 +0200

    Added Azure db connection and storing tokens

---
+++ MVCCoreApp/Migrations/20170603113229_Inttial.Designer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MVCCoreApp.Models;
+
+namespace MVCCoreApp.Migrations
+{
+    [DbContext(typeof(MVCCoreAppContext))]
+    [Migration("20170603113229_Inttial")]
+    partial class Inttial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("MVCCoreApp.Models.Person", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("CreationTime");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Person");
+                });
+        }
+    }
+}
